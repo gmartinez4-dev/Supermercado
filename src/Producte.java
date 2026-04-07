49862786cfba16a301e73503820dc7261ea1cde5
Gerard Martínez Gonzalez <g.martinez4@sapalomera.cat>
2026-04-07 22:21:48 +0200

    Update Producte.java

--- src/Producte.java
+++ src/Producte.java
@@ -1,20 +1,27 @@
+// Classe abstracta Producte que implementa Comparable
 public abstract class Producte implements Comparable<Producte> {
+
+    // Atributs protegits (accessibles des de subclasses)
     protected String nom;
     protected double preu;
     protected String codiBarres;
 
+    // Constructor de la classe Producte
     public Producte(String nom, double preu, String codiBarres) {
         this.nom = nom;
         this.preu = preu;
         this.codiBarres = codiBarres;
     }
 
+    // Getters: permeten obtenir els valors dels atributs
     public String getNom() { return nom; }
     public double getPreu() { return preu; }
     public String getCodiBarres() { return codiBarres; }
 
+    // Mètode abstracte que obligarà les subclasses a definir com es calcula el preu
     public abstract double calcularPreu();
 
+    // Setters: permeten modificar els valors dels atributs
     public void setNom(String nom) {
         this.nom = nom;
     }
@@ -27,7 +34,8 @@ public abstract class Producte implements Comparable<Producte> {
         this.codiBarres = codiBarres;
     }
 
-    // 🔥 IMPLEMENTACIÓN DE COMPARABLE (orden natural por nombre)
+    // Implementació de Comparable:
+    // Defineix l'ordre natural dels productes segons el nom (ordre alfabètic)
     @Override
     public int compareTo(Producte altre) {
         return this.nom.compareTo(altre.nom);
